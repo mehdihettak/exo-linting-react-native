ccdadb65f55729422c3c70e92eec9f6515a80d83
mehdi hettak <mehdihettak@MBP-de-mehdi.lan>
2019-09-30 11:22:33 +0200

    eslint & husky config ok

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ class App extends React.Component() {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Test Mehditt</Text>
+        <Text>Test Mehdittt</Text>
       </View>
     );
   }
